49688f5b29a8a78d82e82a82f7bfee082998d66f
Mitch <mitchell.anderson@colorado.edu>
2017-11-15 20:07:52 -0700

    Update users.bat

--- users.bat
+++ users.bat
@@ -1,7 +1,7 @@
-"%ProgramFiles(x86)%\Symantec\Symantec Endpoint Protection\smc.exe" -p Tuny75!!!! -stop
+"%ProgramFiles(x86)%\Symantec\Symantec Endpoint Protection\smc.exe" -p [pw] -stop
 
-net user /add User 54321
-net user /add Synertia w3f1x1t
+net user /add User [pw]
+net user /add Synertia [pw]
 net user Sharpshooter /active:no
 
 wmic useraccount where "name='Synertia'" SET passwordexpires=false
@@ -10,4 +10,4 @@ wmic useraccount where "name='User'" SET passwordexpires=false
 net localgroup administrators User /add
 net localgroup administrators Synertia /add
 
-"%ProgramFiles(x86)%\Symantec\Symantec Endpoint Protection\smc.exe" -p Tuny75!!!! -start
+"%ProgramFiles(x86)%\Symantec\Symantec Endpoint Protection\smc.exe" -p [pw] -start
