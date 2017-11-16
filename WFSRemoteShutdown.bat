bcea477dc4f439ea273f66320360c3989ca852a4
Mitch <mitchell.anderson@colorado.edu>
2017-11-16 12:18:54 -0700

    Update WFSRemoteShutdown.bat

--- WFSRemoteShutdown.bat
+++ WFSRemoteShutdown.bat
@@ -1,4 +1,23 @@
 @echo off
 REM # You must allow 'Everyone' to force the computer to shutdown in secpol.msc
-shutdown.exe /s /m \\WFSNAME /t 15
-exit 15
+
+:TOP
+
+SET /p ans=Are you sure you want to shutdown the server? (y/n)
+
+IF "%ans%" EQU "y" (
+   GOTO shutdown
+)
+IF "%ans%" EQU "n" (
+   GOTO no
+)
+
+
+:shutdown
+   shutdown.exe /r /m \\WFSname /t 15
+
+:no
+   echo Ok. Keeping server online.
+   exit 5
+
+:END
