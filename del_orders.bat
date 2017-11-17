a0eeb60df138436f3c12720e3624a9fb819137ca
Mitch <mitchell.anderson@colorado.edu>
2017-11-17 16:16:47 -0700

    Update del_orders.bat

--- del_orders.bat
+++ del_orders.bat
@@ -1,3 +1,3 @@
 del C:\ProgramData\Phoenix\Storage\orders\completed\*.*
 del c:\programdata\phoenix\storage\orders\produce\completed\*.*
-rmdir C:\Users\manderson\Desktop\tet
+rmdir C:\Users\user\Desktop\tet
