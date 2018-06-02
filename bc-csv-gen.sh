e923c157ca3f861facbf20268e8f8ad66d342a53
Mitch <mitchell.anderson@colorado.edu>
2018-06-01 18:56:37 -0600

    added SQL query generation

--- bc-csv-gen.sh
+++ bc-csv-gen.sh
@@ -20,3 +20,8 @@ sleep 10
 while read L; do
   echo "('$L', '$1 $2 code', 0.00, $price, 1374077422, 1735929001, 1, 0, 0.00, 1, 0)," >> $1-$2-$WRITEDATE-Full-CSV\.csv
 done <$1-$2-$WRITEDATE-plain-barcodes\.csv
+echo "insert into DB_NAME.coupons (coupon, description, percent, dollar, date_add, date_end, max_redemptions, redemptions, min_order, free_shipping, min_qty)
+
+values" > base-query.txt
+cat base-query.txt $1-$2-$WRITEDATE-Full-CSV\.csv > $1-$2-SQL-Query.txt
+echo "SQL Query Generated: $1-$2-SQL-Query.txt"
