4175b03d39fba147eb49671c5414c93c65346b58
Mitch <mitchell.anderson@colorado.edu>
2018-06-08 13:44:31 -0600

    Update offlineuploadtransfer.bat

--- offlineuploadtransfer.bat
+++ offlineuploadtransfer.bat
@@ -1,4 +1,5 @@
 REM run off USB
+REM Map WFS storage dir to U: on local computer first
 for /R  U:\ %%f in (*.json) do copy %%f "%~d0"
 for /R  U:\ %%f in (*.jpg) do copy %%f "%~d0"
 for /R  U:\ %%f in (*.jar) do copy %%f "%~d0"
